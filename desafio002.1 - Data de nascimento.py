f616a8377cb15c460e0d620062ab62722e1f1cb4
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 08:43:16 -0300

    Crie um programa que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada

--- desafio002.1 - Data de nascimento.py
+++ desafio002.1 - Data de nascimento.py
@@ -3,8 +3,8 @@ Crie um programa que leia o dia, o mês e o ano de nascimento de uma pessoa e mo
 formatada (mensagem de saida = Você nasceu no dia x de x de x. Correto?)"""
 
 # Solução 1
-nasc = input('Quando você nasceu? ')
-print('Você nasceu em', nasc, 'Correto?')
+nasci = input('Quando você nasceu? ')
+print('Você nasceu em', nasci, 'Correto?')
 
 # Solução 2
 dia = input('Em qual dia você nasceu? ')
