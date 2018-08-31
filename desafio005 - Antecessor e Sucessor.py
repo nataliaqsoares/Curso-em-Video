a98b4cd4151fc93fc96c78862abaeb0723352753
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 09:03:24 -0300

    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor

--- desafio005 - Antecessor e Sucessor.py
+++ desafio005 - Antecessor e Sucessor.py
@@ -2,4 +2,4 @@
 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor"""
 
 n = int(input('Digite um número: '))
-print('O número {} tem como sucessor {} e antecessor {}'.format(n, n+1, n-1))
+print('O número {} tem como sucessor {} e antecessor {}.'.format(n, n+1, n-1))
