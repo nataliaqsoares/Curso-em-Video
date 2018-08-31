e7f808f7f5bc45f6f0edbde602854397049c86b7
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 09:04:59 -0300

    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

--- desafio007 - Media aritmetica.py
+++ desafio007 - Media aritmetica.py
@@ -4,4 +4,4 @@ Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a su
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
-print('A média das notas {} mais {} é de {}'.format(nota1, nota2, media))
+print('A média das notas {} mais {} é de {}.'.format(nota1, nota2, media))
