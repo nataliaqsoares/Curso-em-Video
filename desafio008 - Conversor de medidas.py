72d5a0e31862702b8c67d13da9e87cc97c69618b
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 09:05:40 -0300

    Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros

--- desafio008 - Conversor de medidas.py
+++ desafio008 - Conversor de medidas.py
@@ -4,4 +4,4 @@ Escreva um programa que leia um valor em metros e o exiba convertido em centimet
 m = float(input('Informe o valor em metros: '))
 cm = m * 100
 mm = m * 1000
-print('O valor de {}m vale {}cm e {}mm'.format(m, cm, mm))
+print('O valor de {}m vale {}cm e {}mm.'.format(m, cm, mm))
