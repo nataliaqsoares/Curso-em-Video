a7caee1489ad19a6555720c65f2544fd4b6b4095
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 16:47:05 -0300

    Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF
    - Correção do tipo primitivo da variavel c de int para float

--- desafio014 - Conversor de temperaturas.py
+++ desafio014 - Conversor de temperaturas.py
@@ -1,6 +1,6 @@
 """Desafio 014
 Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF"""
 
-c = int(input('Informe a temperatura: '))
+c = float(input('Informe a temperatura ºC: '))
 f = 1.8 * c + 32
 print('{}ºC são {}ºF'.format(c, f))
