7c666394c2472fae39c24932ddf8f2de55c2a729
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 09:10:14 -0300

    Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto

--- desafio012 - Calculando desconto.py
+++ desafio012 - Calculando desconto.py
@@ -1,4 +1,6 @@
 """Desafio 012
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto"""
 
-
+preco = float(input('Informe o preço do produto: '))
+novopreco = preco - (preco * 0.05)
+print('O produto de preço {} com desconto fica por {:.2f}'.format(preco, novopreco))
