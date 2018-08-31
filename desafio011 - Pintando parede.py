cdf717e7593598c9dbfa261695665e62a6a54a35
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-08-31 16:45:44 -0300

    Faça um programa que leia a largura e a altura de uma parade em metros, calcule a sua área e quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²
    - Correção da palavra parede na linha 6
    - Inclusão da limitação de uma casa flutuante para litros na linha 9

--- desafio011 - Pintando parede.py
+++ desafio011 - Pintando parede.py
@@ -2,8 +2,8 @@
 Faça um programa que leia a largura e a altura de uma parade em metros, calcule a sua área e quantidade de tinta
 necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²"""
 
-altura = float(input('Informe a altura da pare: '))
+altura = float(input('Informe a altura da parede: '))
 largura = float(input('Informe a largura da parede: '))
 area = altura * largura
 tinta = area / 2
-print('Para pintar uma parede de {}m² serão necessarios {} litros de tinta'.format(area, tinta))
+print('Para pintar uma parede de {}m² serão necessarios {:.1f} litros de tinta'.format(area, tinta))
