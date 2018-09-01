8c0742f4b6ff62521840d0d49b9f649880ed7026
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-01 12:12:49 -0300

    Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira

--- desafio016 - Quebrando um numero.py
+++ desafio016 - Quebrando um numero.py
@@ -2,3 +2,19 @@
 Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira
 Ex: Digite um numero: 6.127. O número 6.127 tem a parte inteira 6."""
 
+# Solução 1
+import math
+num = float(input('Informe um número real qualquer: '))
+int = math.trunc(num)
+print('O número {} tem a porção inteira de {}'.format(num, int))
+
+# Solução 2
+from math import trunc
+num = float(input('Informe um número real qualquer: '))
+int = trunc(num)
+print('O número {} tem a porção inteira de {}'.format(num, int))
+
+# Solução 3
+num = float(input('Informe um real qualquer: '))
+print('O número {} tem a porção interira de {:.0f}'.format(num, num // 1))
+
