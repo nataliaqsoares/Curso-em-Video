dba20275454b784cb94fc334db42e222e2c0e712
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-05 14:32:53 -0300

    -

---
+++ desafio024 - Verificando as primeiras letras de um texto.py
@@ -0,0 +1,4 @@
+""" Desafio 024
+Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo' """
+
+
---
+++ desafio027 - Primeiro e ultimo nome de uma pessoa.py
@@ -0,0 +1,7 @@
+""" Desafio 027
+Faça um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+Ex.: Ana Maria de Souza
+primeiro = Ana
+último = Souza """
+
+
