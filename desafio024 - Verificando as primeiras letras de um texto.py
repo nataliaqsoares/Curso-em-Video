ed54cc6a194b72ba9c34049304df4e8f03b25bce
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-13 10:57:15 -0300

    Desafio 024 - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo'

--- desafio024 - Verificando as primeiras letras de um texto.py
+++ desafio024 - Verificando as primeiras letras de um texto.py
@@ -1,4 +1,6 @@
 """ Desafio 024
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo' """
 
-
+cidade = input('Informe o nome da sua cidade: ')
+cidade = cidade.split()
+print('Santo' in cidade[0])
