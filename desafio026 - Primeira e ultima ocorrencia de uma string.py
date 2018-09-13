00d407edbddbbde14274142a73fd29af2f7524b1
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-13 11:27:01 -0300

    Desafio 026 - Faça um programa que leia uma frase pelo teclado e mostre: - Quantas vezes aparece a letra 'A' - Em que posição ela aparece a primeira vez - Em que posição ela aparece pela ultima vez

--- desafio026 - Primeira e ultima ocorrencia de uma string.py
+++ desafio026 - Primeira e ultima ocorrencia de uma string.py
@@ -4,4 +4,11 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 - Em que posição ela aparece a primeira vez
 - Em que posição ela aparece pela ultima vez """
 
+frase = input('Digite uma frase: ')
+frase = frase.lower().strip()
+letras = frase.count('a')
+primeiro = frase.find('a')
+ultimo = frase.rfind('a')
+print('A letra A aparece {} vezes na frase. \nA primeira letra A apareceu na posição {}. \nA útima letra A apareceu na'
+      'posição {}.'.format(letras, primeiro + 1, ultimo + 1))
 
