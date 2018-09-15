517166e6ac70333a45c8758d27bc8d6d4cd5f901
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-15 12:26:17 -0300

    Crie um programa que escreva 'Olá, Mundo!' na tela usando emoticons

---
+++ desafio001.1 - Ola Mundo.py
@@ -0,0 +1,8 @@
+"""Desafio 001.1
+Crie um programa que escreva 'Olá, Mundo!' na tela usando emoticons"""
+
+import emoji
+print(emoji.emojize('Olá :earth_americas:!', use_aliases=True))
+print(emoji.emojize('Olá :earth_africa:!', use_aliases=True))
+print(emoji.emojize('Olá :earth_asia:!', use_aliases=True))
+print(emoji.emojize('Olá Mundo! :sunglasses:', use_aliases=True))
