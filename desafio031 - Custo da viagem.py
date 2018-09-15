7f3f9256088ecebdd09a0ea4ea97f501cca4a68a
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-15 14:38:18 -0300

    Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50 por Km para viagens de até 200Km e R$ 0,45 para viagens mais longas.

---
+++ desafio031 - Custo da viagem.py
@@ -0,0 +1,11 @@
+""" Desafio 031
+Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50 por
+Km para viagens de até 200Km e R$ 0,45 para viagens mais longas. """
+
+dist = int(input('Qual a distância da sua viagem? '))
+if dist <= 200:
+    passagem = dist * 0.5
+    print('A passagem para distância de {}Km custa R$ {:.2f}'.format(dist, passagem))
+else:
+    passagem = dist * 0.45
+    print('A passagem para distâncias de {}Km custa R$ {:.2f}'.format(dist, passagem))
