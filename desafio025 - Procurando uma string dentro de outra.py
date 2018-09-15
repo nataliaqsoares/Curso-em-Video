7b13841615cf0d8c5110c50aa304caedbf58dbfb
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-15 13:17:21 -0300

    Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome
    - Alteração: inclusão do str, strip e lower para tratar possíveis problemas de digitação do usuário

--- desafio025 - Procurando uma string dentro de outra.py
+++ desafio025 - Procurando uma string dentro de outra.py
@@ -1,5 +1,5 @@
 """Desafio 025
-Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome """
+Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome """
 
-nome = input('Informe seu nome completo: ')
-print('Silva' in nome)
+nome = str(input('Informe seu nome completo: ')).strip().lower()
+print('silva' in nome)
