d4508badc3e80853dbdc5747a9eb0d2d7a036a5f
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-15 13:39:27 -0300

    Faça um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
    - Alteração: inclusão do str e split junto a variáel para não ter que fazer o tratamento posteriormente

--- desafio027 - Primeiro e ultimo nome de uma pessoa.py
+++ desafio027 - Primeiro e ultimo nome de uma pessoa.py
@@ -4,6 +4,5 @@ Ex.: Ana Maria de Souza
 primeiro = Ana
 último = Souza """
 
-nome = input('Informe seu nome completo: ')
-nome = nome.split()
+nome = str(input('Informe seu nome completo: ')).split()
 print('Seu primeiro nome é {} \nSeu último nome é {}'.format(nome[0], nome[len(nome) - 1]))
