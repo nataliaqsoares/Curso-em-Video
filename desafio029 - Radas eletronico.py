ebeae5a1d68eadba66703c0c41bddd924fa9ca25
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-15 14:18:52 -0300

    Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por km acima do limite.

---
+++ desafio029 - Radas eletronico.py
@@ -0,0 +1,9 @@
+""" Desafio 029
+Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele
+foi multado. A multa vai custar R$ 7,00 por km acima do limite. """
+
+carro = int(input('Informe a velocidade do carro: '))
+if carro > 80:
+    multa = (carro - 80) * 7
+    print('Você foi multado pois ultapassou o limite de velocidade de 80km/h. A multa foi de R$ {:.2f}'.format(multa))
+print('Dirija com segurança!')
