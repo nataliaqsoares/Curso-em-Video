298e964d9cb095f98b8150abab1ed96d7bd7857b
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-17 09:42:34 -0300

    Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
    - Correção: correção da lógica para resolver o problema

--- desafio032 - Ano bissexto.py
+++ desafio032 - Ano bissexto.py
@@ -2,13 +2,7 @@
 Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO """
 
 ano = int(input('Informe o ano que deseja saber se é bissexto: '))
-if ano % 4 == 0:
-    if ano % 100 != 0:
-        print('O ano {} é bissexto'.format(ano))
-    else:
-        print('O ano {} não é bissexto'.format(ano))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é bissexto'.format(ano))
 else:
-    if ano % 400 == 0:
-        print('O ano {} é bissexto'.format(ano))
-    else:
-        print('O ano {} não é bissexto'.format(ano))
+    print('O ano {} não é bissexto'.format(ano))
