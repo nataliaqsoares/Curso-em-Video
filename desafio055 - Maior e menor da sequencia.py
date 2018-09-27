caf20801b408305933770bac3fc48e226b7d4a56
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-27 12:33:37 -0300

    Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.

---
+++ desafio055 - Maior e menor da sequencia.py
@@ -0,0 +1,15 @@
+""" Desafio 055
+Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos. """
+
+peso = float(input('Informe o seu peso: '))
+menor = peso
+maior = peso
+
+for c in range(0, 4):
+    peso = float(input('Informe o seu peso: '))
+    if peso > maior:
+        maior = peso
+    elif peso < menor:
+        menor = peso
+
+print('Maior peso: {}\nMenor peso: {}'.format(maior, menor))
