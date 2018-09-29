4c27910fc95d2e1fd14d49f8245c8444099f364e
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-29 17:59:32 -0300

    Refaça o Defasio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progreção usando a estrutura while.

---
+++ desafio061 - Progressao aritmetica v2.0.py
@@ -0,0 +1,17 @@
+""" Desafio 061
+Refaça o Defasio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progreção usando
+a estrutura while. """
+
+inicio = int(input('Informe o primeiro termo da progressão aritmetica: '))
+razao = int(input('Informe a razão da progressão: '))
+pa = cont = 0
+
+while cont < 9:
+    if cont == 0:
+        pa = inicio + razao
+        cont += 1
+        print('{}º termo: {}'.format(cont, inicio))
+    else:
+        pa += razao
+        cont += 1
+    print('{}º termo: {}'.format(cont + 1, pa))
