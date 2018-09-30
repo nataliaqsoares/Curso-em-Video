a8ec4b4463f6d826490639593df3f1a70126273c
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-09-30 12:44:41 -0300

    Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.

---
+++ desafio062 - Super progressao aritmedica v3.0.py
@@ -0,0 +1,20 @@
+""" Desafio 062
+Melhore o Desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele
+disser que quer mostrar 0 termos. """
+
+inicio = int(input('Informe o primeiro termo: '))
+razao = int(input('Informe a razao: '))
+termo = inicio
+termos = cont = 0
+mais = 10
+
+while mais != 0:
+    print('{}º termo: {}'.format(termos + 1, termo))
+    termo += razao
+    termos += 1
+    cont += 1
+    if cont == mais:
+        print('-' * 32)
+        mais = int(input('Deseja ver mais quantos termos? '))
+        cont = 0
+print('Programa finalizado')
