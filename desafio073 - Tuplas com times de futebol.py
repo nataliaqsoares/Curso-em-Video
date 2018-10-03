982212cbe2788a900b545596d5eb6c42ea5d2571
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-10-03 13:11:24 -0300

    Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre: A) apenas os 5 primeiros colocados; B) os ultimos 4 colocados da tabela; C) uma lista com os times em ordem alfabética; D) em que posição na tabela está o time da chapecoense

---
+++ desafio073 - Tuplas com times de futebol.py
@@ -0,0 +1,16 @@
+""" Desafio 073
+Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de
+colocação. Depois mostre: a) apenas os 5 primeiros colocados; b) os ultimos 4 colocados da tabela; c) uma lista com os
+times em ordem alfabética; d) em que posição na tabela está o time da chapecoense """
+
+times = ('Palmeiras', 'Internacional', 'São Paulo', 'Grêmio', 'Flamengo', 'Atlético Mineiro', 'Cruzeiro', 'Santos',
+         'Corinthians', 'Fluminense', 'Atlético-PR', 'Botafogo', 'América-MG', 'Bahia', 'Ceará', 'Vasco da Gama',
+         'Vitória', 'Chapecoense', 'Sport Recife', 'Paraná')
+print('-' * 50)
+print(f'Os cinco primeiros colocados da tabela são: {times[:6]}')
+print('-' * 50)
+print(f'Os quatro últimos colocados da tabela são: {times[:-5:-1]}')
+print('-' * 50)
+print('Os times em ordem alfabetica são: {} '.format(sorted(times)))
+print('-' * 50)
+print('A Chapecoense está na {}º colocação'.format(times.index('Chapecoense')+1))
