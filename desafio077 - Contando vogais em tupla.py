c89b988eb4c6d0a6c66b1030f7fd598c95db3b47
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-10-05 15:02:17 -0300

    Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais

---
+++ desafio077 - Contando vogais em tupla.py
@@ -0,0 +1,12 @@
+""" Desafio 077
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada
+palavra, quais são as suas vogais """
+
+palavras = ('Aprender', 'Programar', 'Linguagem', 'Python', 'Curso', 'Gratis', 'Estudar', 'Praticar', 'Trabalhar',
+            'Mercado', 'Programador', 'Futuro')
+for palavra in palavras:
+    print(f'Na palavra {palavra} temos as vogais: ', end='')
+    for letra in palavra:
+        if letra in 'AaEeIiOoUu':
+            print(f'{letra}', end=' ')
+    print('\n')
