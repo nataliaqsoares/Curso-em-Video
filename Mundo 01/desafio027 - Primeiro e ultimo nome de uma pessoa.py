d5e61f12286f275b702ef0efd2588a68eef84454
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:10:02 -0200

    Faça um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. Ex.: Ana Maria de Souza Primeiro = Ana Último = Souza

--- Mundo 01/desafio027 - Primeiro e ultimo nome de uma pessoa.py
+++ Mundo 01/desafio027 - Primeiro e ultimo nome de uma pessoa.py
@@ -1,8 +1,8 @@
 """ Desafio 027
 Faça um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 Ex.: Ana Maria de Souza
-primeiro = Ana
-último = Souza """
+Primeiro = Ana
+Último = Souza """
 
 nome = str(input('Informe seu nome completo: ')).split()
 print('Seu primeiro nome é {} \nSeu último nome é {}'.format(nome[0], nome[len(nome) - 1]))
