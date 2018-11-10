8342504d4991520c9712eb23428527a4438617f8
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:04:19 -0200

    Crie um programa que leia o nome completo de uma pessoa e mostre: - O nome com todas as letras maiúsculas; - O nome com todas minúsculas; - Quantas letras tem ao todosem considerar espaços; - Quantas letras tem o primeiro nome;

--- Mundo 01/desafio022 - Analisador de textos.py
+++ Mundo 01/desafio022 - Analisador de textos.py
@@ -2,8 +2,8 @@
 Crie um programa que leia o nome completo de uma pessoa e mostre:
 - O nome com todas as letras maiúsculas;
 - O nome com todas minúsculas;
-- Quantas letras tem ao todosem considerar espaços
-- Quantas letras tem o primeiro nome """
+- Quantas letras tem ao todosem considerar espaços;
+- Quantas letras tem o primeiro nome; """
 
 nome = str(input('Informe seu nome completo: ')).strip()
 maiusculo = nome.upper()
