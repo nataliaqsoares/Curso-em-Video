f1eeae3cdd65542b3641841af93b3241259d50a3
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:52:17 -0200

    Crie um programa que leia dois valores e mostre um menu na tela: [1] somar; [2] multiplicar; [3] maior; [4] novos números [5] sair do programa; Seu programa devera realizar a operação solicitada em cada caso

--- Mundo 02/desafio059 - Criando um menu de opcoes.py
+++ Mundo 02/desafio059 - Criando um menu de opcoes.py
@@ -1,6 +1,6 @@
 """ Desafio 059
 Crie um programa que leia dois valores e mostre um menu na tela: [1] somar; [2] multiplicar; [3] maior; [4] novos números
-[5] sair do programa; Seu programa devera realizar a operação solicitada em cada caso. """
+[5] sair do programa; Seu programa devera realizar a operação solicitada em cada caso """
 
 num1 = int(input('Informe um valor: '))
 num2 = int(input('Informe mais um valor: '))
