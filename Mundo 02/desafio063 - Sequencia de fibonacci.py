6c6e172914ab2ef27478edf3772cd96a98775538
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:56:00 -0200

    Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci. Ex.: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8

--- Mundo 02/desafio063 - Sequencia de fibonacci.py
+++ Mundo 02/desafio063 - Sequencia de fibonacci.py
@@ -3,9 +3,12 @@ Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n
 Fibonacci. Ex.: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8 """
 
 termo = int(input('Deseja ver quantos termos da sequência de fibonacci: '))
+
 cont = n = 1
 fibo = 0
+
 print('0')
+
 while termo != cont:
     n = n + fibo
     fibo = n - fibo
