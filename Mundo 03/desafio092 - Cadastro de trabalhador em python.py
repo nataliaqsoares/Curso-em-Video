7e0f59cb3d5311e4dbdcbef2b2125dfa9480998a
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:32:26 -0200

    Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario se por acaso a CTPS for diferente de zero, o dicionario receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar. Obs.: Considerar que a pessoa se aposenta depois de 35 anos de contribuição

--- Mundo 03/desafio092 - Cadastro de trabalhador em python.py
+++ Mundo 03/desafio092 - Cadastro de trabalhador em python.py
@@ -2,7 +2,7 @@
 Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario se
 por acaso a CTPS for diferente de zero, o dicionario receberá também o ano de contratação e o salário. Calcule e
 acrescente, além da idade, com quantos anos a pessoa vai se aposentar. Obs.: Considerar que a pessoa se aposenta depois
-de 35 anos de contribuição."""
+de 35 anos de contribuição """
 
 from datetime import date
 
@@ -13,6 +13,7 @@ prev['Nome'] = str(input('Nome: '))
 ano = int(input('Ano de Nascimento: '))
 prev['Idade'] = ano_atual - ano
 prev['CTPS'] = int(input('Carteira de Trabalho (0 caso não tenha): '))
+
 if prev['CTPS'] != 0:
     prev['Contratacao'] = int(input('Ano de contratação: '))
     prev['Salario'] = float(input('Salário: R$ '))
