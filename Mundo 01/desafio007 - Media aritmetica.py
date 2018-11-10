48d5f77df887f1a2e1dc50e49d32a660ee0369a7
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:49:45 -0200

    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

--- Mundo 01/desafio007 - Media aritmetica.py
+++ Mundo 01/desafio007 - Media aritmetica.py
@@ -1,5 +1,5 @@
-"""Desafio 007
-Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média"""
+""" Desafio 007
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média """
 
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
