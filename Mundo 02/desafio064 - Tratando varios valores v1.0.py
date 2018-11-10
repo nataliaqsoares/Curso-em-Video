38a6d7b5f1447ce7445a6b615d9c989969ba6d0b
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:56:28 -0200

    Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag)

--- Mundo 02/desafio064 - Tratando varios valores v1.0.py
+++ Mundo 02/desafio064 - Tratando varios valores v1.0.py
@@ -1,7 +1,7 @@
 """ Desafio 064
 Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor
 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
-(desconsiderando o flag)"""
+(desconsiderando o flag) """
 
 num = int(input('Informe um número ou 999 para parar: '))
 soma = cont = 0
@@ -10,4 +10,5 @@ while num != 999:
     soma += num
     cont += 1
     num = int(input('Informe um número ou 999 para parar: '))
+
 print('Foram digitados {} números e a soma entre eles é {}'.format(cont, soma))
