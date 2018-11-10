ae15f1295f0dbd09fab98fa3175acd8ffcf16483
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:57:10 -0200

    Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF

--- Mundo 01/desafio014 - Conversor de temperaturas.py
+++ Mundo 01/desafio014 - Conversor de temperaturas.py
@@ -1,5 +1,5 @@
-"""Desafio 014
-Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF"""
+""" Desafio 014
+Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF """
 
 c = float(input('Informe a temperatura ºC: '))
 f = 1.8 * c + 32
