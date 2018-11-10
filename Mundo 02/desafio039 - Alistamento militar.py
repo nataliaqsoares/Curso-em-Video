01907ffb513a07f935c25f3b6b1d4c5b36427637
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:25:17 -0200

    Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade: - Se ele ainda vai se alistar ao serviço militar 'Faltam x anos para você se alistar'; - Se é a hora de se alistar 'Se aliste agora'; - Se já passou do tempo do alistamento 'Já passaram x anos do seu alistamento'; Seu programa também deverá mostrar o tempo que falta ou que passou do prazo

--- Mundo 02/desafio039 - Alistamento militar.py
+++ Mundo 02/desafio039 - Alistamento militar.py
@@ -2,8 +2,8 @@
 Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
 - Se ele ainda vai se alistar ao serviço militar 'Faltam x anos para você se alistar';
 - Se é a hora de se alistar 'Se aliste agora';
-- Se já passou do tempo do alistamento 'Já passaram x anos do seu alistamento'.
-Seu programa também deverá mostrar o tempo que falta ou que passou do prazo"""
+- Se já passou do tempo do alistamento 'Já passaram x anos do seu alistamento';
+Seu programa também deverá mostrar o tempo que falta ou que passou do prazo """
 
 from datetime import date
 
