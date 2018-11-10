3a01f96ee371464fb7bce7ef329beb7c7caf3707
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:21:21 -0200

    Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta

--- Mundo 03/desafio086 - Matriz em Python.py
+++ Mundo 03/desafio086 - Matriz em Python.py
@@ -1,8 +1,9 @@
 """ Desafio 086
 Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a
-matriz na tela, com a formatação correta. """
+matriz na tela, com a formatação correta """
 
 matriz = [[], [], []]
+
 c = cont = 0
 
 while True:
