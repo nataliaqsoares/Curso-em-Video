572b1098499689f7665d12b47ec0237aade1f50c
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:46:54 -0200

    Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.

--- Mundo 01/desafio002 - Respondendo ao Usuario.py
+++ Mundo 01/desafio002 - Respondendo ao Usuario.py
@@ -1,5 +1,5 @@
-"""Desafio 002
-Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado."""
+""" Desafio 002
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado. """
 
 nome = input('Qual o seu nome? ')
 print('Olá', nome, 'Seja bem-vindo(a)!!')
