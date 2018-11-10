60c2994943804cc8e1229113fc9a65645c4b2ab9
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:11:13 -0200

    Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista

--- Mundo 03/desafio078 - Maior e menor valores na lista.py
+++ Mundo 03/desafio078 - Maior e menor valores na lista.py
@@ -1,6 +1,6 @@
 """ Desafio 078
 Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor
-valor digitado e as suas respectivas posições na lista."""
+valor digitado e as suas respectivas posições na lista """
 
 valores = list()
 
