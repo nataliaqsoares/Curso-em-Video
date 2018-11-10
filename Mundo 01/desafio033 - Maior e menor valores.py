d2bd0e257d309491be8d7edc726d0abc6c3ebd63
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:14:48 -0200

    Faça um programa que leia três números e mostre qual é o maior e qual é o menor.

--- Mundo 01/desafio033 - Maior e menor valores.py
+++ Mundo 01/desafio033 - Maior e menor valores.py
@@ -4,8 +4,10 @@ Faça um programa que leia três números e mostre qual é o maior e qual é o m
 n1 = int(input('Informe um número: '))
 n2 = int(input('Informe mais um número: '))
 n3 = int(input('Informe mais um número: '))
+
 maior = 0
 menor = 0
+
 if n1 > n2 and n1 > n3:
     maior = n1
 if n2 > n1 and n2 > n3:
@@ -18,4 +20,5 @@ if n2 < n1 and n2 < n3:
     menor = n2
 if n3 < n1 and n3 < n2:
     menor = n3
+
 print('Dos três números informados o maior número é {} e o menor número é {}'.format(maior, menor))
