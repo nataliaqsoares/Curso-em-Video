58ae9e46c110de607bd83d8741cd66d9804970ff
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:51:38 -0200

    Melhore o jogo do Desafio 028 onde o computador vai 'pensar' em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer

--- Mundo 02/desafio058 - Jogo de Adivinhacao v2.0.py
+++ Mundo 02/desafio058 - Jogo de Adivinhacao v2.0.py
@@ -1,12 +1,14 @@
 """ Desafio 058
 Melhore o jogo do Desafio 028 onde o computador vai 'pensar' em um número entre 0 e 10. Só que agora o jogador vai
-tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer. """
+tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer """
 
 from random import randint
 
 pc = randint(0, 10)
+
 print('Estou pensando em um número entre 0 e 10... Tente adivinhar!')
 jog = int(input('Em qual número pensei? '))
+
 cont = 1
 
 while pc != jog:
