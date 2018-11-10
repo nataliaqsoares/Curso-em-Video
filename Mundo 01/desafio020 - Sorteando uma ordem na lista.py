0759c294bbd2850eb753f1a5a4c81062df173dd0
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:00:51 -0200

    O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada

--- Mundo 01/desafio020 - Sorteando uma ordem na lista.py
+++ Mundo 01/desafio020 - Sorteando uma ordem na lista.py
@@ -1,6 +1,6 @@
-"""Desafio 020
+""" Desafio 020
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa
-que leia o nome dos quatro alunos e mostre a ordem sorteada"""
+que leia o nome dos quatro alunos e mostre a ordem sorteada """
 
 # Solução 1
 import random
