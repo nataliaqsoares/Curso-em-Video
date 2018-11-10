dc3dddc23922e1346821d41c4ff49166c21abe4e
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:50:58 -0200

    Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado peça a digítação novamente até ter um valor correto

--- Mundo 02/desafio057 - Validacao de dados.py
+++ Mundo 02/desafio057 - Validacao de dados.py
@@ -1,8 +1,10 @@
 """ Desaio 057
 Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado peça a digítação
-novamente até ter um valor correto."""
+novamente até ter um valor correto """
 
 sexo = str(input('Informe o seu sexo: [M/F]')).strip().lower()[0]
+
 while sexo != 'm' and sexo != 'f':
     sexo = str(input('Sexo informado não aceito pelo sistema. Informe seu sexo novamente: [M/F]')).strip().lower()
+
 print('Sexo registrado com sucesso')
