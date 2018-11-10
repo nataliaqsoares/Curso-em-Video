4e71076bc57eecdf089ab2c378e029f2c048462d
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:55:06 -0200

    Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar. Considere US$ 1,00 = R$ 3,27

--- Mundo 01/desafio010 - Conversor de moedas.py
+++ Mundo 01/desafio010 - Conversor de moedas.py
@@ -1,6 +1,6 @@
-"""Desafio 010
+""" Desafio 010
 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
-Considere US$ 1,00 = R$ 3,27"""
+Considere US$ 1,00 = R$ 3,27 """
 
 reais = float(input('Informe quantos reais tem na carteira: '))
 dolares = reais / 3.27
