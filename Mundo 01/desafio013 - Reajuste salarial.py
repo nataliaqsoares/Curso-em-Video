805528ce96582a8351ab94c40ce69c468d4a623e
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:56:43 -0200

    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento

--- Mundo 01/desafio013 - Reajuste salarial.py
+++ Mundo 01/desafio013 - Reajuste salarial.py
@@ -1,5 +1,5 @@
-"""Desafio 013
-Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento"""
+""" Desafio 013
+Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento """
 
 salario = float(input('Informe o seu salário: '))
 aumento = salario * 0.15
