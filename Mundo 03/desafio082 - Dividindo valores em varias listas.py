281b94dd4e29ddfd1628c321b1eeb224fe05b9d1
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:15:57 -0200

    Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas

--- Mundo 03/desafio082 - Dividindo valores em varias listas.py
+++ Mundo 03/desafio082 - Dividindo valores em varias listas.py
@@ -1,7 +1,7 @@
 """ Desafio 082
 Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter
 apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas
-geradas. """
+geradas """
 
 valores = list()
 par = list()
