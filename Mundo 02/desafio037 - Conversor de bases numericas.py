c86f18081417027a5e96f1aa9220cfd2b4c4be3a
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:22:47 -0200

    Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1: para binário; 2: para octal; 3: para hexadecimal;

--- Mundo 02/desafio037 - Conversor de bases numericas.py
+++ Mundo 02/desafio037 - Conversor de bases numericas.py
@@ -1,6 +1,6 @@
 """ Desafio 037
-Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
-1: para binário; 2: para octal; 3: para hexadecimal; """
+Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1:
+ para binário; 2: para octal; 3: para hexadecimal; """
 
 num = int(input('Informe o número que deseja converter: '))
 base = int(input('Informe para qual base deseja converter:\n[ 1 ] Binário\n[ 2 ] Octal\n[ 3 ] Hexadecimal\n'))
