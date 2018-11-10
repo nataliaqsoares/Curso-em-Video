9c1645aed339c88b44f6a28c7e7e695684560a08
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:00:20 -0200

    Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.

--- Mundo 01/desafio019 - Sorteando um item na lista.py
+++ Mundo 01/desafio019 - Sorteando um item na lista.py
@@ -1,6 +1,6 @@
-"""Desafio 019
+""" Desafio 019
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome
-deles e escrevendo o nome escolhido."""
+deles e escrevendo o nome escolhido. """
 
 # Solução 1
 
