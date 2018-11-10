1ba3f694b9e8e3c521d1dbe7ee369befe9524061
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:36:38 -0200

    Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo e realize a contagem. Seu programa tem que realizar três contagens através da função criada: a) De 1 até 10, de 1 em 1; b) De 10 até 0, de 2 em 2; c) Uma contagem personalizada. Obs.: Se o passo for negativo, passar para positivo; Se o passo for 0, trocar para 1

--- Mundo 03/desafio098 - Funcao de contador.py
+++ Mundo 03/desafio098 - Funcao de contador.py
@@ -2,7 +2,7 @@
 Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo e realize a
 contagem. Seu programa tem que realizar três contagens através da função criada: a) De 1 até 10, de 1 em 1; b) De 10 até
 0, de 2 em 2; c) Uma contagem personalizada. Obs.: Se o passo for negativo, passar para positivo; Se o passo for 0,
-trocar para 1"""
+trocar para 1 """
 
 from time import sleep
 
