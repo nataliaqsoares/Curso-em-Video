533a083d2ff9c07a6551a9c595ba930da928c54e
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:35:51 -0200

    Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável. Obs.: Ajustar os traços ao tamanho da palavra

--- Mundo 03/desafio097 - Um print especial.py
+++ Mundo 03/desafio097 - Um print especial.py
@@ -1,6 +1,6 @@
 """ Desafio 097
 Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma
-mensagem com tamanho adaptável. Obs.: Ajustar os traços ao tamanho da palavra"""
+mensagem com tamanho adaptável. Obs.: Ajustar os traços ao tamanho da palavra """
 
 
 def escreva(msg):
