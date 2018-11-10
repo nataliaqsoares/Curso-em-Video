7b9a816b1070f92d3d9c843c71a840b4beacdee7
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:07:15 -0200

    Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome

--- Mundo 01/desafio025 - Procurando uma string dentro de outra.py
+++ Mundo 01/desafio025 - Procurando uma string dentro de outra.py
@@ -1,4 +1,4 @@
-"""Desafio 025
+""" Desafio 025
 Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome """
 
 nome = str(input('Informe seu nome completo: ')).strip().lower()
