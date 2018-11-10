b3d15fc6cec2dcecb317cf714e69368721818ba6
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:08:16 -0200

    Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla

--- Mundo 03/desafio074 - Maior e menor valores com tupla.py
+++ Mundo 03/desafio074 - Maior e menor valores com tupla.py
@@ -1,10 +1,11 @@
 """ Desafio 074
 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de
-números gerados e também indique o menor e o maior valor que estão na tupla. """
+números gerados e também indique o menor e o maior valor que estão na tupla """
 
 from random import randint
 
 conjunto = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
 print('Os números sorteados foram: ', end='')
 
 for num in conjunto:
