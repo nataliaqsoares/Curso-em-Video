07138cdd2b0295169c6893cd61c042f5da63f3ea
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:46:39 -0200

    Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores. Considerar a maioridade com 21 anos

--- Mundo 02/desafio054 - Grupo da maioridade.py
+++ Mundo 02/desafio054 - Grupo da maioridade.py
@@ -1,6 +1,6 @@
 """ Desafio 054
 Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a
-maioridade e quantas já são maiores. Considerar a maioridade com 21 anos"""
+maioridade e quantas já são maiores. Considerar a maioridade com 21 anos """
 
 from datetime import date
 
