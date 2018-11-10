ce9a8c42185c92d6fc2afd1a235f4f5b4706384e
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:26:33 -0200

    Aprimore o desafio anterior, mostrando no final: a) a soma de todos os valores pares digitados b) a soma dos valores da terceira coluna c) o maior valor da segunda linha

--- Mundo 03/desafio087 - Mais sobre matriz em Python.py
+++ Mundo 03/desafio087 - Mais sobre matriz em Python.py
@@ -1,6 +1,6 @@
 """ Desafio 087
 Aprimore o desafio anterior, mostrando no final: a) a soma de todos os valores pares digitados b) a soma dos valores da
-terceira coluna c) o maior valor da segunda linha. """
+terceira coluna c) o maior valor da segunda linha """
 
 matriz = [[], [], []]
 somapar = somacol = c = cont = 0
@@ -16,11 +16,13 @@ while True:
         somacol += n
     if cont == 9:
         break
+
 print('=' * 40)
 for conj in matriz:
     for n in conj:
         print(f'[{n:^5}]', end=' ')
     print()
+
 print('=' * 40)
 print(f'A soma dos números pares é igual a {somapar}.\nA soma dos números da terceira coluna é igual a {somacol}.\n'
       f'O maior valor da segunda linha é {max(matriz[1])}.')
