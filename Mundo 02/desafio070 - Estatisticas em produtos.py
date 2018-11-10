858b3a1034633c155095a309daa588bbeed8aa69
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:00:45 -0200

    Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre: A) Qual é o total gasto na compra. B) Quantos produtos custam mais de R$ 1000. C) Qual é o nome do produto mais barato

--- Mundo 02/desafio070 - Estatisticas em produtos.py
+++ Mundo 02/desafio070 - Estatisticas em produtos.py
@@ -1,7 +1,7 @@
 """ Desafio 070
 Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar.
 No final, mostre: A) Qual é o total gasto na compra. B) Quantos produtos custam mais de R$ 1000. C) Qual é o nome do
-produto mais barato. """
+produto mais barato """
 
 gasto = caro = barato = cont = 0
 
@@ -20,5 +20,6 @@ while True:
     continua = str(input('Deseja continuar: [S/N] ')).strip().lower()[0]
     if continua == 'n':
         break
+
 print(f'O total gasto nessa compra foi de R$ {gasto:.2f}\n{caro} custam mais de R$ 1000.00\nO produto mais barato foi '
       f'{produto_barato} que custou R$ {barato:.2f}')
