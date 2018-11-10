d98d6d06bd228a02bdc65ba1054045d235b350c7
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:24:11 -0200

    Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem: - O primeiro valor é maior; - O segundo valor é maior; - Não existe valor maior, os dois são iguais.

--- Mundo 02/desafio038 - Comparando numeros.py
+++ Mundo 02/desafio038 - Comparando numeros.py
@@ -1,6 +1,6 @@
 """ Desafio 038
 Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem: - O primeiro valor é
-maior; - O segundo valor é maior; - Não existe valor maior, os dois são iguais """
+maior; - O segundo valor é maior; - Não existe valor maior, os dois são iguais. """
 
 n1 = int(input('Informe um número: '))
 n2 = int(input('Informe mais um número: '))
