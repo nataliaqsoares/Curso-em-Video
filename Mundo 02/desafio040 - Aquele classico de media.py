e5fda3434066affcd72c5a253bbfcd79dbe0c600
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:26:12 -0200

    Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: -Média abaixo de 5.0: reprovado - Média entre 5.0 e 6.9: recuperação -Média 7.0 ou superior: aprovado

--- Mundo 02/desafio040 - Aquele classico de media.py
+++ Mundo 02/desafio040 - Aquele classico de media.py
@@ -1,6 +1,6 @@
 """ Desafio 040
 Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a
-média atingida: -Média abaixo de 5.0: reprovado -Média entre 5.0 e 6.9: recuperação -Média 7.0 ou superior: aprovado"""
+média atingida: -Média abaixo de 5.0: reprovado - Média entre 5.0 e 6.9: recuperação -Média 7.0 ou superior: aprovado"""
 
 nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
