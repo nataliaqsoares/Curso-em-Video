e6d21bc4605d1c1fab950cd1a6842517c14ee7bc
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:03:43 -0200

    Faça um programa que abra e reprodruza o áudio de um arquivo mp3

--- Mundo 01/desafio021 - Tocando um mp3.py
+++ Mundo 01/desafio021 - Tocando um mp3.py
@@ -1,9 +1,9 @@
 """ Desafio 021
-Faça um programa que abra e reprodruza o áudio de um arquivo mp3"""
+Faça um programa que abra e reprodruza o áudio de um arquivo mp3 """
 
 import os
 
-os.startfile('C:\\Users\\Natália\\Desktop\\Python\\Curso em Video\\desafios\\unity.mp3')
+os.startfile('C:\\Users\\Natália\\Desktop\\Python\\Curso em Video\\desafios\\Mundo 01\\unity.mp3')
 
-os.startfile(r'C:\Users\Natália\Desktop\Python\Curso em Video\desafios\unity.mp3')
+os.startfile(r'C:\Users\Natália\Desktop\Python\Curso em Video\desafios\Mundo 01\unity.mp3')
 
