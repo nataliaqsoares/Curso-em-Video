b8c368ef6af5a538a0e5aabed1945128cb60b03b
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:46:04 -0200

    Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Ex.: Apos a sopa / A sacada da casa / A torre da derrota / O lobo ama o bolo / Anotaram a data da maratona

--- Mundo 02/desafio053 - Detector de palindromo.py
+++ Mundo 02/desafio053 - Detector de palindromo.py
@@ -1,8 +1,9 @@
 """ Desafio 053
 Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
-Ex.: Apos a sopa / A sacada da casa / A torre da derrota / O lobo ama o bolo / Anotaram a data da maratona"""
+Ex.: Apos a sopa / A sacada da casa / A torre da derrota / O lobo ama o bolo / Anotaram a data da maratona """
 
 frase = str(input('Informe uma frase: ')).lower().split()
+
 frase = ''.join(frase)
 cont_frase = len(frase)-1
 cont = 0
@@ -11,6 +12,7 @@ for c in range(0, len(frase)):
     if frase[c] == frase[cont_frase]:
         cont += 1
     cont_frase -= 1
+
 if len(frase) == cont:
     print('Essa frase é um palíndromo')
 else:
