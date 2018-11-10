0091668457127c26bb3a48aedd578aea9dcb4cfa
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:44:49 -0200

    Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão

--- Mundo 02/desafio051 - Progressao aritmetica.py
+++ Mundo 02/desafio051 - Progressao aritmetica.py
@@ -1,9 +1,10 @@
 """ Desafio 051
 Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa
-progressão. """
+progressão """
 
 inicio = int(input('Digite o primeiro termo da progressão: '))
 razao = int(input('Digite a razão para essa progressão: '))
+
 fim = inicio + (11 - 1) * razao
 contador = 1
 
