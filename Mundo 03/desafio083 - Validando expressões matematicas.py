44e5250d92f0279881e1096981a9592aa55f80df
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 15:18:08 -0200

    Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta

--- Mundo 03/desafio083 - Validando expressões matematicas.py
+++ Mundo 03/desafio083 - Validando expressões matematicas.py
@@ -4,9 +4,11 @@ expressão passada está com os parênteses abertos e fechados na ordem correta 
 
 cont = 0
 exp = str(input('Digite a sua expressão:'))
+
 for c in exp:
     if c == ')' or c == '(':
         cont += 1
+
 if cont % 2 == 0:
     print('A sua expressão está certa')
 else:
