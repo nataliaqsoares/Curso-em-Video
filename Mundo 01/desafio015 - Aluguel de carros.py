8af99096ed8a6195b65c914c305af854cf7393b1
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 13:57:36 -0200

    Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por Km rodado

--- Mundo 01/desafio015 - Aluguel de carros.py
+++ Mundo 01/desafio015 - Aluguel de carros.py
@@ -1,6 +1,6 @@
-"""Desafio 015
+""" Desafio 015
 Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais
-ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por Km rodado"""
+ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por Km rodado """
 
 km = float(input('Quantos km foram percorridos? '))
 dias = int(input('Quantos dias o carro ficou alugado? '))
