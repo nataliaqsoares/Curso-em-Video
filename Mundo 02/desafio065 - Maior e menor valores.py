20e38d499bd7102e531a8299af2a58277724a851
Natalia Quintanilha <natalia.analise@yahoo.com>
2018-11-10 14:57:11 -0200

    Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores

--- Mundo 02/desafio065 - Maior e menor valores.py
+++ Mundo 02/desafio065 - Maior e menor valores.py
@@ -1,7 +1,7 @@
 """ Desafio 065
 Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os
 valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar
-a digitar valores."""
+a digitar valores """
 
 continuar = 's'
 maior = menor = soma = cont = 0
@@ -17,5 +17,6 @@ while continuar != 'n':
     elif num < menor:
         menor = num
     continuar = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
+
 print('A média dos {} números digitados é {}\nO maior valor foi {} e o menor foi {}'
       .format(cont, (soma / cont), maior, menor))
